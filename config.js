402cfd46e810429d9eb8f383af88261b9dcbb247
Mac Hale <machaledesign@gmail.com>
2020-09-28 17:11:55 -0600

    installs dependencies

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 8080,
-  DATABASE_URL: process.env.DATABASE_URL || 'development',
-};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
-const app = require('./src/app');
 const { PORT, DATABASE_URL } = require('./config');
+const app = require('./app');
 const knex = require('knex');
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
 });
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at https://localhost:${PORT}`);
